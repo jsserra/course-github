37ccb306adb1124f01640cb791bc307b4d8f1040
Juliano Sales <jsserra21@gmail.com>
2018-12-14 20:22:20 -0200

    variavel soma

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,12 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                 System.out.println("Teste GitHub");
+                 double num1 = 2.00;
+                 double num2 = 3.00;
+                 double sum;
+                 sum = num1 + num2;
+                 System.out.printf("Soma: %.2f", sum);
+                
 
 	}
 
